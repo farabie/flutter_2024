350dbf093ffda5b1da441cfe570631d6f52d8a7b
Muhammad Farabie <farabie024@gmail.com>
2024-07-25 11:52:28 +0700

    Text Widget & Text Decoration

--- lib/ui/pages/text_widget_tutorial.dart
+++ lib/ui/pages/text_widget_tutorial.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TextWidgetTutorial extends StatelessWidget {
   const TextWidgetTutorial({super.key});
@@ -10,7 +11,35 @@ class TextWidgetTutorial extends StatelessWidget {
         title: const Text("Text Widget Tutorial"),
       ),
       body: const Center(
-        child: Text("Text Widget Tutorial"),
+        child: SizedBox(
+          width: 200,
+          child: Text(
+            "Hello My Nama is Muhammad Farabie & I Study Flutter Tutorial 2024",
+            textAlign: TextAlign.left,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              // backgroundColor: Colors.red,
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+              shadows: [
+                Shadow(
+                  blurRadius: 0,
+                  color: Colors.black,
+                  offset: Offset(3, 3)
+                )
+              ],
+              letterSpacing: 0.5,
+              wordSpacing: 2,
+              decoration: TextDecoration.underline,
+              decorationThickness: 1,
+              decorationColor: Colors.red,
+              decorationStyle: TextDecorationStyle.wavy,
+            ),
+          ),
+        ),
       ),
     );
   }
